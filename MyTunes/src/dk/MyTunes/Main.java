976286a1efc7cb12f1aedfa668c3375656861101
Jean-Jensen <jeanjensen911@gmail.com>
2023-11-28 02:09:38 +0700

    .

--- MyTunes/src/dk/MyTunes/Main.java
+++ MyTunes/src/dk/MyTunes/Main.java
@@ -1,10 +1,13 @@
 package dk.MyTunes;
 
+import dk.MyTunes.DAL.SongsDAO;
 import dk.MyTunes.GUI.FXML.GraphicalGUI;
 
 
 public class Main {
     public static void main(String[] args) {
+        SongsDAO dao = new SongsDAO();
+        dao.test();
         GraphicalGUI.run();
     }
 
