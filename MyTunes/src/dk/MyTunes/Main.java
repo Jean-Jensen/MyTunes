25d6b7ee9624d13307da7b0b6241422afe5d06c4
Jean-Jensen <jeanjensen911@gmail.com>
2023-11-28 03:14:20 +0700

    Merged my branch with brandons & edited minor things

--- MyTunes/src/dk/MyTunes/Main.java
+++ MyTunes/src/dk/MyTunes/Main.java
@@ -1,9 +1,6 @@
 package dk.MyTunes;
 
 import com.microsoft.sqlserver.jdbc.SQLServerException;
-import dk.MyTunes.BE.Song;
-import dk.MyTunes.DAL.ConnectionManager;
-import dk.MyTunes.DAL.SongsDAO;
 import dk.MyTunes.GUI.FXML.GraphicalGUI;
 
 
