59066d39e8fcfc3edf8ef4a96f60a0fd74b701ff
Taja992 <Tharaxui@gmail.com>
2023-12-01 12:00:49 +0100

    -Added Playlist Foundations

---
+++ MyTunes/src/dk/MyTunes/BLL/BLLPlaylist.java
@@ -0,0 +1,41 @@
+package dk.MyTunes.BLL;
+
+import dk.MyTunes.BE.Playlist;
+import dk.MyTunes.BE.Song;
+import dk.MyTunes.DAL.PlaylistDAO;
+import dk.MyTunes.Exceptions.MyTunesExceptions;
+
+import java.util.List;
+
+
+public class BLLPlaylist {
+    private PlaylistDAO playlistDAO = new PlaylistDAO();
+
+    public void createPlaylist(String name) throws MyTunesExceptions {
+        playlistDAO.createPlaylist(name);
+    }
+
+    public List<Playlist> getAllPlaylists() throws MyTunesExceptions {
+        return playlistDAO.getAllPlaylists();
+    }
+
+    public void updatePlaylist(Playlist p) throws MyTunesExceptions {
+        playlistDAO.updatePlaylist(p);
+    }
+
+    public void deletePlaylist(int id) throws MyTunesExceptions {
+        playlistDAO.deletePlaylist(id);
+    }
+
+    public void addSongToPlaylist(int songId, int playlistId) throws MyTunesExceptions {
+        playlistDAO.addSongToPlaylist(songId, playlistId);
+    }
+
+    public void removeSongFromPlaylist(int songId, int playlistId) throws MyTunesExceptions {
+        playlistDAO.removeSongFromPlaylist(songId, playlistId);
+    }
+
+    public List<Song> getSongsInPlaylist(int playlistId) throws MyTunesExceptions {
+        return playlistDAO.getSongsInPlaylist(playlistId);
+    }
+}
